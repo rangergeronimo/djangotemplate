e0750f60da61fbf4cb7e2c08b3d17ba9327c97cc
rangergeronimo <rangergeronimo@gmail.com>
2020-05-16 16:34:21 -0400

    fist commit

---
+++ project_name/settings/development.py
@@ -0,0 +1,27 @@
+from .base import *
+
+# Configure remote domain name
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
+
+DEBUG = True
+
+# installed apps 
+INSTALLED_APPS += []
+
+
+#Database Setup
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_NAME'],
+        'PASSWORD': os.environ['DB_NAME'],
+        'HOST': os.environ['DB_NAME'],
+        'PORT': int(os.environ['DB_PORT'])
+    }
+}
+
+# static files setup 
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [ BASE_DIR, 'staticfiles']
